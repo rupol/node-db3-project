6dc8d1b3d145bcec64736a462397fd268a5e342c
Ruth Poliakon <r.poliakon@gmail.com>
2020-01-07 18:45:56 -0700

    stretch - multi table queries

--- queries.sql
+++ queries.sql
@@ -19,3 +19,14 @@ ORDER BY p.ProductName;
 SELECT o.Id AS "Order", c.CompanyName AS "Company", e.LastName AS "Employee" FROM "Order" AS o
 JOIN Customer AS c ON o.CustomerId = c.Id
 JOIN Employee AS e ON o.EmployeeId = e.Id;
+
+-- STRETCH --
+
+-- Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 9 records.
+SELECT COUNT(p.CategoryID) AS "Count", c.CategoryName FROM Categories as c
+JOIN Products AS p ON c.CategoryID = p.CategoryID
+GROUP BY c.CategoryName;
+
+-- Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+SELECT COUNT(ProductID) AS "ItemCount", OrderID FROM OrderDetails
+GROUP BY OrderID;
